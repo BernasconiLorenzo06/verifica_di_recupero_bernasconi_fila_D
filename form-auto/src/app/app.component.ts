13f8b81da669c387d6eebedcf5c514aeef829586
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2024-02-05 12:38:24 +0000

    punto 6

--- form-auto/src/app/app.component.ts
+++ form-auto/src/app/app.component.ts
@@ -6,10 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  vettore: any[];
+  constructor() {
+    this.vettore = [];
+  }
+      ngOnInit(): void {
+      }
   autoTot(colore: HTMLInputElement, porte: HTMLInputElement, auto: HTMLInputElement): boolean {
-    console.log (colore.value,porte.value,auto.value); 
+   
+    console.log (colore.value,porte.value,auto.value);
+   
+    this.vettore.push(colore.value + " " + porte.value + " " + auto.value)
+    console.log(this.vettore)
     return false;
-
   }
 }
